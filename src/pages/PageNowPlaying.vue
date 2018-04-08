cae23533e932dc3b41b98ee8f9d1adc2fc195f1e
chme <christian.meffert@googlemail.com>
2018-04-08 09:29:03 +0200

    Force 100% width for range slider

--- src/pages/PageNowPlaying.vue
+++ src/pages/PageNowPlaying.vue
@@ -108,13 +108,11 @@ export default {
 .seek-slider {
   min-width: 250px;
   max-width: 500px;
-  width: 100%;
+  width: 100% !important;
 }
 .seek-slider .range-slider-fill {
   background-color: hsl(171, 100%, 41%);
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-}
-.seek-slider .range-slider-rail {
 }
 .seek-slider .range-slider-knob {
   width: 10px;
